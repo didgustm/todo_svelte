d5856777c14177ffc85919ebc4ab23e678070dc0
yanghyunyangha <bw03306@gmail.com>
2023-04-28 14:46:35 +0900

    hosting

--- src/components/remain.svelte
+++ src/components/remain.svelte
@@ -2,10 +2,8 @@
     export let styles, remain
 </script>
 
-{#if remain > 1}
-<p class="{ styles.remain }"><span>{remain}</span> items left</p>
-{:else if remain == 1}
-<p class="{ styles.remain }"><span>{remain}</span> item left</p>
+{#if remain >= 1}
+<p class="{ styles.remain }"><span>{remain}</span> {remain > 1? 'items': 'item'} left</p>
 {:else}
 <p class="{ styles.remain }">No item</p>
 {/if}
